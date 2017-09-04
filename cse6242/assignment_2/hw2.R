3b4effa96870a810c7abac650954dbd74bda5023
Vincent La <vincela14@gmail.com>
2017-09-04 00:36:25 -0700

    almost finished hw2

--- cse6242/assignment_2/hw2.R
+++
@@ -1,9 +0,0 @@
-require(ggplot2)
-data(midwest)
-
-df = midwest[, c('PID', 'county', 'state', 'percprof')]
-print(df)
-summary(df)
-
-ggplot(df, aes(x=percprof)) +
-  geom_density(aes(color=state, group=state))
