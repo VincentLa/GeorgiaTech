b6c17241a0171e6757b9667ff8a7974b134fa93a
Vincent La <vincent.la@cloverhealth.com>
2018-01-14 15:39:57 -0800

    adding csvs

--- cs7646/ML4T_2018Spring/assess_portfolio/analysis.py
+++ cs7646/ML4T_2018Spring/assess_portfolio/analysis.py
@@ -10,13 +10,14 @@ import numpy as np
 import datetime as dt
 from util import get_data, plot_data
 
+
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality
-def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
-    syms = ['GOOG','AAPL','GLD','XOM'], \
-    allocs=[0.1,0.2,0.3,0.4], \
-    sv=1000000, rfr=0.0, sf=252.0, \
-    gen_plot=False):
+def assess_portfolio(sd=dt.datetime(2008, 1, 1), ed = dt.datetime(2009, 1, 1), \
+                     syms=['GOOG', 'AAPL', 'GLD', 'XOM'], \
+                     allocs=[0.1, 0.2, 0.3, 0.4], \
+                     sv=1000000, rfr=0.0, sf=252.0, \
+                     gen_plot=False):
 
     # Read in adjusted closing prices for given symbols, date range
     dates = pd.date_range(sd, ed)
@@ -24,6 +25,10 @@ def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
     prices = prices_all[syms]  # only portfolio symbols
     prices_SPY = prices_all['SPY']  # only SPY, for comparison later
 
+
+    print(prices.head())
+
+    print('adding code next')
     # Get daily portfolio value
     port_val = prices_SPY # add code here to compute daily portfolio values
 
@@ -52,7 +57,7 @@ def test_code():
     end_date = dt.datetime(2010,1,1)
     symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
     allocations = [0.2, 0.3, 0.4, 0.1]
-    start_val = 1000000  
+    start_val = 1000000
     risk_free_rate = 0.0
     sample_freq = 252
 
