14f9f9f92910cb9b1e89c5a611acac6a0467fdac
Vincent La <vincela14@gmail.com>
2018-03-11 14:20:27 -0700

    Adding final pass

--- cs7646/ML4T_2018Spring/marketsim/marketsim.py
+++ cs7646/ML4T_2018Spring/marketsim/marketsim.py
@@ -11,6 +11,7 @@ PYTHONPATH=../:. python grade_marketsim.py
 Using https://www.youtube.com/watch?v=TstVUVbu-Tk as a guide
 
 Extra Point: https://www.youtube.com/watch?v=TstVUVbu-Tk 1:03:00
+June 15th, 2011, ignore all orders.
 """
 
 import pandas as pd
@@ -31,24 +32,14 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, c
 
     # Read in Orders file
     orders = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan'])
-    orders.reset_index(inplace=True)
 
     # Get Start and End Date
-    start_date = orders.Date.min()
-    end_date = orders.Date.max()
-    # start_date = dt.date(2011, 1, 10)
-    # end_date = dt.date(2011, 12, 20)
+    start_date = orders.index.min()
+    end_date = orders.index.max()
 
     di = {'BUY': 1, 'SELL': -1}
     orders['order_direction'] = orders.Order.map(di)
     orders['actual_shares'] = orders.Shares * orders.order_direction
-    # If there are transactions that net each other on the same day, net them out
-    orders_transformed = orders.groupby(['Date', 'Symbol'])['actual_shares'].sum().to_frame()
-    orders_transformed.reset_index(inplace=True)
-    orders_transformed = orders_transformed.pivot(
-        index='Date', columns='Symbol', values='actual_shares')
-    orders_transformed = orders_transformed.fillna(0)
-
 
     # Get all unique stocks in the orders file
     stocks = list(orders.Symbol.unique())
@@ -56,55 +47,45 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, c
     # Read in Prices Data for all the stocks in the orders file
     prices = get_data(stocks, pd.date_range(start_date, end_date))
     prices = prices[stocks]  # remove SPY
+    prices['cash'] = 1.0
 
     # Create a copy of the prices DF
     # 1. Fill all values with 0's (About 26:50 in YT Video)
     # 2. Fill in values. Each Stock column represents change in position. Cash column represents change in cash
     #    (About 28:00 in YT Video)
     trades = prices.copy()
-
     trades = trades.replace(trades, 0)
-    trades.update(orders_transformed)
-    trade_values = trades.multiply(prices)
-    # -1 because the cash you get is "opposite" of the order
-    # For example, if you buy 1000 shares, you lose -1000 shares worth of cash
-    trades['cash'] = trade_values.sum(axis=1) * -1
+
+    # Loop through orders DF to update Trades
+    for index, row in orders.iterrows():
+        ticker = row.Symbol
+        price = prices.loc[index, ticker]
+
+        # Extra Point: https://www.youtube.com/watch?v=TstVUVbu-Tk 1:03:00
+        # June 15th, 2011, ignore all orders.
+        if index != dt.datetime(2011, 6, 15, 0, 0, 0):
+            trades.loc[index, ticker] += row.actual_shares
+            if row.Order == 'BUY':
+                new_price = price * (1 + impact)
+                trades.loc[index, 'cash'] += (-1 * new_price * row.actual_shares) - commission
+            if row.Order == 'SELL':
+                new_price = price * (1 - impact)
+                trades.loc[index, 'cash'] += (-1 * new_price * row.actual_shares) - commission
 
     # Now compute portfolio value daily
     # The value for each day is cash plus the current value of equities.
     portvals = trades.copy()
     portvals = portvals.cumsum()
 
-    # Add a Cash column for prices
-    prices['cash'] = 1.0
     portvals = portvals.multiply(prices)
     portvals['portfolio_value'] = portvals.sum(axis=1)
     portvals = portvals[['portfolio_value']]
+
+    # Adding the starting value at the end
+    # Shouldn't matter in this case that we add it in the end. Even if we assume 0 cash at the beginning
+    # Because we don't have to worry about leverage it's fine.
     portvals['portfolio_value'] = portvals.portfolio_value + start_val
 
-    # rv = pd.DataFrame(index=portvals.index, data=portvals.as_matrix())
-
-    # Create dataframe of Prices
-    print('printing Trades')
-    print(trades)
-    print(trades.index)
-
-    print('prices')
-    print(prices)
-    print(prices.index)
-
-    # print('portvals')
-    # print(portvals)
-
-    # print('printing rv')
-    # print(rv.head())
-
-    print('printing orders')
-    print(orders.head())
-    print(orders_transformed.head())
-    print(orders_transformed.index)
-
-    # return rv
     return portvals
 
 
@@ -114,6 +95,12 @@ def test_code():
     # Define input parameters
 
     of = "./orders/orders-short.csv"
+    orders = pd.read_csv(of, index_col='Date', parse_dates=True, na_values=['nan'])
+
+    # Get Start and End Date
+    start_date = orders.index.min()
+    end_date = orders.index.max()
+
     sv = 1000000
 
     # Process orders
@@ -125,9 +112,24 @@ def test_code():
     
     # Get portfolio stats
     # Here we just fake the data. you should use your code from previous assignments.
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]
+    last_day = portvals.shape[0] - 1
+    cum_ret = portvals[last_day] / portvals[0] - 1
+
+    # Daily Return = daily_portfolio_value[t] / daily_portfolio_value[t - 1] - 1
+    # Taken from: https://classroom.udacity.com/courses/ud501/lessons/4156938722/concepts/41858589820923
+    daily_return = portvals / portvals.shift(1) - 1
+    
+    # Average Daily Return
+    avg_daily_ret = daily_return.mean()
+
+    # Standard deviation of daily return
+    std_daily_ret = daily_return.std()
+
+    rfr = 0
+    sf = 252
+    sharpe_ratio = (daily_return - rfr).mean() / (daily_return - rfr).std()
+    sharpe_ratio *= np.sqrt(sf)
+
     cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
 
     # Compare portfolio against $SPX
