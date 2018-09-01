1c6d6d9b7e0c8ca9dff2fb73bea626bcbbabd9a8
Vincent La <vincent.la@cloverhealth.com>
2018-08-31 17:13:21 -0700

    Finishing cross validation

--- cse6250/903178639-vla6-hw1/src/cross.py
+++ cse6250/903178639-vla6-hw1/src/cross.py
@@ -1,3 +1,10 @@
+"""
+Author: Vincent La
+vla6
+
+To test: nosetests tests/test_cross_validation.py
+"""
+
 import models_partc
 from sklearn.model_selection import KFold, ShuffleSplit
 from numpy import mean
@@ -12,31 +19,49 @@ RANDOM_STATE = 545510477
 #input: training data and corresponding labels
 #output: accuracy, auc
 def get_acc_auc_kfold(X,Y,k=5):
-	#TODO:First get the train indices and test indices for each iteration
-	#Then train the classifier accordingly
-	#Report the mean accuracy and mean auc of all the folds
-	return None,None
+    #TODO:First get the train indices and test indices for each iteration
+    #Then train the classifier accordingly
+    #Report the mean accuracy and mean auc of all the folds
+    kf = KFold(n_splits=k, random_state=RANDOM_STATE)
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+
+        y_pred = models_partc.logistic_regression_pred(X_train, y_train, X_test)
+        accuracy, area_under_curve, precision, recall, f1score = models_partc.classification_metrics(
+            Y_pred=y_pred, Y_true=y_test)
+    return accuracy, area_under_curve
 
 
 #input: training data and corresponding labels
 #output: accuracy, auc
 def get_acc_auc_randomisedCV(X,Y,iterNo=5,test_percent=0.2):
-	#TODO: First get the train indices and test indices for each iteration
-	#Then train the classifier accordingly
-	#Report the mean accuracy and mean auc of all the iterations
-	return None,None
+    #TODO: First get the train indices and test indices for each iteration
+    #Then train the classifier accordingly
+    #Report the mean accuracy and mean auc of all the iterations
+    sp = ShuffleSplit(n_splits=iterNo, random_state=RANDOM_STATE, test_size=test_percent)
+
+    for train_index, test_index in sp.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+
+        y_pred = models_partc.logistic_regression_pred(X_train, y_train, X_test)
+        accuracy, area_under_curve, precision, recall, f1score = models_partc.classification_metrics(
+            Y_pred=y_pred, Y_true=y_test)
+    return accuracy, area_under_curve
 
 
 def main():
-	X,Y = utils.get_data_from_svmlight("../deliverables/features_svmlight.train")
-	print("Classifier: Logistic Regression__________")
-	acc_k,auc_k = get_acc_auc_kfold(X,Y)
-	print(("Average Accuracy in KFold CV: "+str(acc_k)))
-	print(("Average AUC in KFold CV: "+str(auc_k)))
-	acc_r,auc_r = get_acc_auc_randomisedCV(X,Y)
-	print(("Average Accuracy in Randomised CV: "+str(acc_r)))
-	print(("Average AUC in Randomised CV: "+str(auc_r)))
+    X,Y = utils.get_data_from_svmlight("../deliverables/features_svmlight.train")
+    print("Classifier: Logistic Regression__________")
+    acc_k,auc_k = get_acc_auc_kfold(X,Y)
+    print(("Average Accuracy in KFold CV: "+str(acc_k)))
+    print(("Average AUC in KFold CV: "+str(auc_k)))
+    acc_r,auc_r = get_acc_auc_randomisedCV(X,Y)
+    print(("Average Accuracy in Randomised CV: "+str(acc_r)))
+    print(("Average AUC in Randomised CV: "+str(auc_r)))
 
 if __name__ == "__main__":
-	main()
+    main()
 
