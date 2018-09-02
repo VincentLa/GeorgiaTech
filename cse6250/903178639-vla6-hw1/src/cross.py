2d7f7b25ea276d4181121c355d58494706e862b0
Vincent La <vincent.la@cloverhealth.com>
2018-09-02 12:41:40 -0700

    Fixing cross

--- cse6250/903178639-vla6-hw1/src/cross.py
+++ cse6250/903178639-vla6-hw1/src/cross.py
@@ -8,6 +8,7 @@ To test: nosetests tests/test_cross_validation.py
 import models_partc
 from sklearn.model_selection import KFold, ShuffleSplit
 from numpy import mean
+import numpy as np
 
 import utils
 
@@ -23,6 +24,8 @@ def get_acc_auc_kfold(X,Y,k=5):
     #Then train the classifier accordingly
     #Report the mean accuracy and mean auc of all the folds
     kf = KFold(n_splits=k, random_state=RANDOM_STATE)
+    accuracy_array = np.array([])
+    area_under_curve_array = np.array([])
 
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -31,7 +34,9 @@ def get_acc_auc_kfold(X,Y,k=5):
         y_pred = models_partc.logistic_regression_pred(X_train, y_train, X_test)
         accuracy, area_under_curve, precision, recall, f1score = models_partc.classification_metrics(
             Y_pred=y_pred, Y_true=y_test)
-    return accuracy, area_under_curve
+        accuracy_array = np.append(accuracy_array, accuracy)
+        area_under_curve_array = np.append(area_under_curve_array, area_under_curve)
+    return np.mean(accuracy_array), np.mean(area_under_curve_array)
 
 
 #input: training data and corresponding labels
@@ -41,6 +46,8 @@ def get_acc_auc_randomisedCV(X,Y,iterNo=5,test_percent=0.2):
     #Then train the classifier accordingly
     #Report the mean accuracy and mean auc of all the iterations
     sp = ShuffleSplit(n_splits=iterNo, random_state=RANDOM_STATE, test_size=test_percent)
+    accuracy_array = np.array([])
+    area_under_curve_array = np.array([])
 
     for train_index, test_index in sp.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -49,7 +56,9 @@ def get_acc_auc_randomisedCV(X,Y,iterNo=5,test_percent=0.2):
         y_pred = models_partc.logistic_regression_pred(X_train, y_train, X_test)
         accuracy, area_under_curve, precision, recall, f1score = models_partc.classification_metrics(
             Y_pred=y_pred, Y_true=y_test)
-    return accuracy, area_under_curve
+        accuracy_array = np.append(accuracy_array, accuracy)
+        area_under_curve_array = np.append(area_under_curve_array, area_under_curve)
+    return np.mean(accuracy_array), np.mean(area_under_curve_array)
 
 
 def main():
