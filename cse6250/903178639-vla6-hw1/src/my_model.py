0abea43c72d055648a141d375a5ae138f6b6a1d3
Vincent La <vincent.la@cloverhealth.com>
2018-09-02 14:37:23 -0700

    Passing all tests now

--- cse6250/903178639-vla6-hw1/src/my_model.py
+++ cse6250/903178639-vla6-hw1/src/my_model.py
@@ -1,3 +1,10 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import dump_svmlight_file
+
 import etl
 import utils
 #Note: You can reuse code that you wrote in etl.py and models.py and cross.py over here. It might help.
@@ -17,14 +24,159 @@ Save the file as "test_features.txt" and save it inside the folder deliverables
 input:
 output: X_train,Y_train,X_test
 '''
-def my_features():
-	"""
-	Generate own features. As a first pass, just replicate what we've already done in 
-	"""
+def calculate_index_date(events):
+    """
+    Similar to calculating index date in etl.py, except now we don't have mortality DF.
+    """
+    # Aggregate the events DF to get last event date for each patient
+    indx_date = events[['patient_id', 'timestamp']].groupby(['patient_id'])\
+                                                   .agg({'timestamp': 'max'})\
+                                                   .rename(columns={'timestamp': 'last_event_date'})\
+                                                   .reset_index()
+    indx_date.last_event_date = pd.to_datetime(indx_date.last_event_date)
+    indx_date['indx_date'] = indx_date.last_event_date
+    indx_date = indx_date[['patient_id', 'indx_date']]
+    return indx_date
 
-	X_train, Y_train = utils.get_data_from_svmlight("../deliverables/features_svmlight.train")  # Need to replace this with my own
-	X_test, Y_test = utils.get_data_from_svmlight("../data/features_svmlight.validate")
-	return X_train, Y_train, X_test
+def filter_events(events, indx_date):
+    
+    '''
+    Similar to calculating filter events in etl.py but now we aren't writing to a file
+    '''
+    # Define beginning and end of observation window and prediction window, respectively
+    indx_date['observation_window_begin'] = indx_date.indx_date - pd.to_timedelta(2000, unit='d')
+    indx_date['prediction_window_end'] = indx_date.indx_date + pd.to_timedelta(30, unit='d')
+
+    # Merge
+    filtered_events = events.merge(indx_date, how='left', on='patient_id')
+    filtered_events.timestamp = pd.to_datetime(filtered_events.timestamp)
+
+    filtered_events['keep_event'] = (filtered_events.timestamp >= filtered_events.observation_window_begin) &\
+                                    (filtered_events.timestamp <= filtered_events.indx_date)
+    filtered_events = filtered_events.loc[filtered_events.keep_event]
+    filtered_events = filtered_events[['patient_id', 'event_id', 'value']]
+
+    return filtered_events
+
+def aggregate_events(filtered_events_df, feature_map_df):
+    
+    '''
+    Similar to calculating aggregated events in etl.py but now we aren't writing to a file
+    '''
+    # Need to Remove rows where value is null
+    events_remove_na = filtered_events_df.loc[filtered_events_df.value.isnull() == False]
+
+    events_remove_na = events_remove_na.merge(feature_map_df, how='left', on='event_id')
+    events_remove_na.rename(columns={'idx': 'event_id_idx'}, inplace=True)
+    events_remove_na['event_type'] = events_remove_na.event_id.str.replace('[^a-zA-Z]', '')
+
+    events_grouped = events_remove_na[['patient_id',
+                                       'event_id_idx',
+                                       'event_type',
+                                       'value']].groupby(['patient_id', 'event_id_idx', 'event_type'])\
+                                                .agg({'value': ['count', 'sum']})
+    
+    # Neat trick to collapse the hierarchical columns returned by groupby to single column
+    events_grouped.columns = ['_'.join(col).strip() for col in events_grouped.columns.values]
+
+    # reset index
+    events_grouped.reset_index(inplace=True)
+
+    events_grouped['value'] = np.where(events_grouped['event_type'] == 'LAB',
+                                                      events_grouped.value_count,
+                                                      events_grouped.value_sum)
+
+    aggregated_events = events_grouped[['patient_id', 'event_id_idx', 'value']]
+    aggregated_events = aggregated_events.rename(columns={'event_id_idx': 'feature_id', 'value': 'feature_value'})
+
+    aggregated_events['patient_id'] = aggregated_events.patient_id.astype(float)
+    aggregated_events['feature_id'] = aggregated_events.feature_id.astype(float)
+    aggregated_events['feature_value'] = aggregated_events.feature_value.astype(float)
+    
+    # Min Max Normalization
+    feature_min_max = aggregated_events[['feature_id', 'feature_value']].groupby(['feature_id']).agg({'feature_value': ['min', 'max']})
+    feature_min_max.columns = ['_'.join(col).strip() for col in feature_min_max.columns.values]
+    feature_min_max.reset_index(inplace=True)
+
+
+    aggregated_events = aggregated_events.merge(feature_min_max, on=['feature_id'])
+
+    aggregated_events['feature_value_normalized'] = (aggregated_events.feature_value) / (aggregated_events.feature_value_max)
+    aggregated_events = aggregated_events[['patient_id',
+                                           'feature_id',
+                                           'feature_value_normalized']].rename(columns={'feature_value_normalized': 'feature_value'})
+
+    aggregated_events['patient_id'] = aggregated_events.patient_id.astype(float)
+    aggregated_events['feature_id'] = aggregated_events.feature_id.astype(float)
+    aggregated_events['feature_value'] = aggregated_events.feature_value.astype(float)
+
+    return aggregated_events
+
+
+def create_features(events, feature_map):
+    """
+    Create Features, similar to etl.py, but with no mortality
+    """
+
+    #Calculate index date
+    indx_date = calculate_index_date(events)
+
+    #Filter events in the observation window
+    filtered_events = filter_events(events, indx_date)
+    
+    #Aggregate the event values for each patient 
+    aggregated_events = aggregate_events(filtered_events, feature_map)
+    
+    events = events.loc[events.value.isnull() == False]
+
+    # Patient Features
+    aggregated_events.sort_values(['patient_id', 'feature_id'], inplace=True)
+    patient_features = {}
+    patients = aggregated_events.patient_id.drop_duplicates()
+    for p in patients:
+        patient_features[p] = []
+
+    for index, row in aggregated_events.iterrows():
+        patient_features[row.patient_id].append((row.feature_id, row.feature_value))
+
+    return patient_features
+
+
+def write_features(events, feature_map):
+    deliverable1 = open('../deliverables/test_features.txt', 'wb')
+    patient_features = create_features(events, feature_map)
+
+    patients = list(patient_features.keys())
+    patients.sort()
+
+    for patient in patients:
+        features = patient_features[patient]
+        features = sorted(features, key=lambda x: x[0])
+        patient_features[patient] = features
+
+    for patient in patients:
+        deliverable1_text = str(int(patient)) + ' '
+        for features in patient_features[patient]:
+            deliverable1_text += (str(int(features[0])) + ':' + str("%.6f" % features[1]) + ' ')
+        deliverable1.write(bytes(deliverable1_text, 'UTF-8'))
+        deliverable1.write(bytes('\n', 'UTF-8'))
+    deliverable1.close()
+
+
+def my_features():
+    """
+    Generate own features. As a first pass, just replicate what we've already done in etl.py
+    """
+    # First pass basically code above replicates what we've already done in ETL.py for the test data
+    filepath = '../data/test'
+    events = pd.read_csv(os.path.join(filepath, 'events.csv'))
+    feature_map = pd.read_csv(os.path.join(filepath, 'event_feature_map.csv'))
+    write_features(events, feature_map)
+
+    X_train, Y_train = utils.get_data_from_svmlight("../deliverables/features_svmlight.train")
+    X_test, _ = utils.get_data_from_svmlight("../deliverables/test_features.txt")
+
+    return X_train, Y_train, X_test
 
 
 '''
@@ -33,16 +185,22 @@ You can use any model you wish.
 input: X_train, Y_train, X_test
 output: Y_pred
 '''
-def my_classifier_predictions(X_train,Y_train,X_test):
-	#TODO: complete this
-	return None
+def my_classifier_predictions(X_train, Y_train, X_test):
+    """
+    As first pass just use Random Forest
+    """
+    clf = RandomForestClassifier()
+    clf.fit(X_train, Y_train)
+    y_pred = clf.predict(X_test)
+    return y_pred
 
 
 def main():
-	X_train, Y_train, X_test = my_features()
-	Y_pred = my_classifier_predictions(X_train,Y_train,X_test)
-	utils.generate_submission("../deliverables/test_features.txt",Y_pred)
-	#The above function will generate a csv file of (patient_id,predicted label) and will be saved as "my_predictions.csv" in the deliverables folder.
+    X_train, Y_train, X_test = my_features()
+    my_classifier_predictions(X_train,Y_train,X_test)
+    Y_pred = my_classifier_predictions(X_train,Y_train,X_test)
+    utils.generate_submission("../deliverables/test_features.txt",Y_pred)
+    #The above function will generate a csv file of (patient_id,predicted label) and will be saved as "my_predictions.csv" in the deliverables folder.
 
 if __name__ == "__main__":
     main()
