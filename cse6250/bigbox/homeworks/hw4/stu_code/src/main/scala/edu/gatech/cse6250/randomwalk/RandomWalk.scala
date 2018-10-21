5f2bbf00565d95b3eb799fdb3dcbdbc56c59b8ef
Vincent La <vincent.la@cloverhealth.com>
2018-10-21 13:06:20 -0700

    Adding pic

--- cse6250/bigbox/homeworks/hw4/stu_code/src/main/scala/edu/gatech/cse6250/randomwalk/RandomWalk.scala
+++ cse6250/bigbox/homeworks/hw4/stu_code/src/main/scala/edu/gatech/cse6250/randomwalk/RandomWalk.scala
@@ -11,56 +11,8 @@ object RandomWalk {
      * Return a List of patient IDs ordered by the highest to the lowest similarity.
      * For ties, random order is okay
      */
-    val personalized = patientID
-    val src: VertexId = patientID
 
-    // Initialize the PageRank graph with each edge attribute having
-    // weight 1/outDegree and each vertex with attribute resetProb.
-    // When running personalized pagerank, only the source vertex
-    // has an attribute resetProb. All others are set to 0.
-    var rankGraph: Graph[Double, Double] = graph
-      // Associate the degree with each vertex
-      .outerJoinVertices(graph.outDegrees) { (vid, vdata, deg) => deg.getOrElse(0) }
-      // Set the weight on the edges based on the degree
-      .mapTriplets(e => 1.0 / e.srcAttr, TripletFields.Src)
-      // Set the vertex attributes to the initial pagerank values
-      .mapVertices { (id, attr) =>
-        if (!(id != src)) alpha else 0.0
-      }
-
-    def delta(u: VertexId, v: VertexId): Double = { if (u == v) 1.0 else 0.0 }
-
-    var iteration = 0
-    var prevRankGraph: Graph[Double, Double] = null
-    while (iteration < numIter) {
-      rankGraph.cache()
-
-      // Compute the outgoing rank contributions of each vertex, perform local preaggregation, and
-      // do the final aggregation at the receiving vertices. Requires a shuffle for aggregation.
-      val rankUpdates = rankGraph.aggregateMessages[Double](
-        ctx => ctx.sendToDst(ctx.srcAttr * ctx.attr), _ + _, TripletFields.Src)
-
-      // Apply the final rank updates to get the new ranks, using join to preserve ranks of vertices
-      // that didn't receive a message. Requires a shuffle for broadcasting updated ranks to the
-      // edge partitions.
-      prevRankGraph = rankGraph
-      val rPrb = {
-        (src: VertexId, id: VertexId) => alpha * delta(src, id)
-      }
-
-      rankGraph = rankGraph.joinVertices(rankUpdates) {
-        (id, oldRank, msgSum) => if (id == patientID) alpha + (1.0 - alpha) * msgSum else (1.0 - alpha) * msgSum
-      }.cache()
-
-      rankGraph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
-      //logInfo(s"PageRank finished iteration $iteration.")
-      prevRankGraph.vertices.unpersist(false)
-      prevRankGraph.edges.unpersist(false)
-
-      iteration += 1
-    }
-    val top10 = rankGraph.vertices.filter(f => f._1 <= 1000).takeOrdered(11)(Ordering[Double].reverse.on(x => x._2)).map(_._1)
-
-    top10.slice(1, top10.length).toList
+    /** Remove this placeholder and implement your code */
+    List(1, 2, 3, 4, 5)
   }
 }
