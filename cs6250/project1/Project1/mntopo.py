49695254c064e97e16ccf2d6c3977187e6f60813
VincentLa <vincela14@gmail.com>
2019-05-25 19:38:49 -0400

    Finished first part

--- cs6250/project1/Project1/mntopo.py
+++ cs6250/project1/Project1/mntopo.py
@@ -14,16 +14,24 @@ class MNTopo(Topo):
 
         # Host and link configuration
         hostConfig = {'cpu': cpu}
-        linkConfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
+        linkConfig = {'bw': 50, 'delay': '10ms', 'loss': 0,
                    'max_queue_size': max_queue_size }
 
         # Hosts and switches
         s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')  # Added
+        s3 = self.addSwitch('s3')  # Added
         sender = self.addHost('sender', **hostConfig)
         receiver = self.addHost('receiver', **hostConfig)
 
         # Wire receiver
         self.addLink(receiver, s1, port1=0, port2=1, **linkConfig)
 
+        # Wire s1
+        self.addLink(s1, s2, port1=2, port2=1, **linkConfig)
+
+        # Wire s2
+        self.addLink(s2, s3, port1=2, port2=1, **linkConfig)
+
         # Wire sender
-        self.addLink(sender, s1, port1=0, port2=2, **linkConfig)
+        self.addLink(sender, s3, port1=0, port2=2, **linkConfig)
